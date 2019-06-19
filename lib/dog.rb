83be4fc563fe0b6d430cabc9a627f0e009c53619
Eric Clark <eclark0013@gmail.com>
2019-06-19 21:28:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
 class Dog
+  def name=(input_name)
+    @dog_name=input_name
+
+  end
+
+  def name
+    @dog_name
+
+  end
+
+  def breed=(input_breed)
+    @dog_breed=input_breed
+
+  end
+
+  def breed
+    @dog_breed
+
+  end
 end
